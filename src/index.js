425e5d0a5e8139272a4990f7e601e6358e1e8860
Mariya <mariaomware@gmail.com>
2024-04-12 12:55:05 +0300

    Complete challenge

--- src/index.js
+++ src/index.js
@@ -1,22 +1,59 @@
 // index.js
-
+document.addEventListener("DOMContentLoaded", function () {
+  main();
+});
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  const ramenDetail = document.getElementById('ramen-detail');
+  ramenDetail.innerHTML = `
+  <img src="${ramen.image}" alt="${ramen.name}" />
+  <h2>${ramen.name}</h2>
+  <p>${ramen.comment}</p>
+  <p>Rating: ${ramen.rating}</p>
+  `;
 };
 
 const addSubmitListener = () => {
-  // Add code
-}
+  const form = document.getElementById('new-ramen');
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('new-name').value;
+    const image = document.getElementById('new-image').value;
+    const rating = document.getElementById('new-rating').value;
+    const comment = document.getElementById('new-comment').value;
+
+    const ramen = { name, image, rating, comment };
+    displayNewRamen(ramen);
+  });
+};
 
 const displayRamens = () => {
-  // Add code
+  fetch('http://localhost:3000/ramens')
+  .then(response => response.json())
+  .then(ramens => {
+    const ramenMenu = document.getElementById('ramen-menu');
+    ramenMenu.innerHTML = '';
+
+    ramens.forEach(ramen => {
+      const img = document.createElement('img');
+      img.src = ramen.image;
+      img.alt = ramen.name;
+      img.addEventListener('click', () => handleClick(ramen));
+      ramenMenu.appendChild(img);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching  ramens:', error);
+  });
 };
 
 const main = () => {
   // Invoke displayRamens here
+  displayRamens();
   // Invoke addSubmitListener here
-}
+  addSubmitListener();
+};
 
 main()
 
